34f9224cabbe865a0f012827b7174eac207d47da
Francois Laberge <francoislaberge@Jibos-MacBook-Pro-2.local>
2017-09-12 07:26:08 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+The Jibo SDK is undergoing revision and will return! Visit https://developers.jibo.com and sign up to stay in the loop about SDK updates.
---
+++ spec/jibo-sdk-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import JiboSdkView from '../lib/jibo-sdk-view';
+
+describe('JiboSdkView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
